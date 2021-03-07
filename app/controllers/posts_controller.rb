bde15a58832911e03127200f915be52916fec3dc
Aaron-Portman <aaronmportman@gmail.com>
2021-03-07 17:57:53 -0500

    Added Post View

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,8 @@ class PostsController < ApplicationController
     def index
         @posts = Post.all
     end
+    def show_post
+        @post = Post.find(params[:id])
+        @user = User.find(@post.user_id)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "posts#index"
+  get "/show_post", to: "posts#show_post"
 end
