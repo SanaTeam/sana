6adc7a2dbb24f7a71deb473880ffc3746f7e15b1
Adam Fleishaker <adamfleishaker@gmail.com>
2021-03-07 20:56:46 -0500

    Fixed message user_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_04_154733) do
+ActiveRecord::Schema.define(version: 2021_03_08_014546) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2021_03_04_154733) do
   create_table "messages", force: :cascade do |t|
     t.string "content"
     t.integer "to"
-    t.integer "from"
+    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
