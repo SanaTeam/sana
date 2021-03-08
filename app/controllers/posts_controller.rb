3389640dfb73877c8ba88aca95e1020aa236d4b8
Adam Fleishaker <adamfleishaker@gmail.com>
2021-03-08 01:11:27 -0500

    Add Bootstrap/FA gem, navbar, flash alerts, apply to views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,12 @@ class PostsController < ApplicationController
     def index
         @posts = Post.all
     end
+
+    def match
+        flash[:notice] = "Match to be implemented soon!"
+        redirect_to posts_path
+    end
+
     def show
         @post = Post.find(params[:id])
         @user = User.find(@post.user_id)
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+    def to_datetime_string(datetime)
+        datetime.strftime("%m/%d/%Y %H:%M %p")
+    end
 end
