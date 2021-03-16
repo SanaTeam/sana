4aa315c60d55f2d10a323667332132ef2c279ae8
Nicole Zamora <nicolebzf@brandeis.edu>
2021-03-15 20:44:10 -0400

    Created Authentication for users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
+    
     include ApplicationHelper
+    include SessionsHelper
+
+    private
+    def logged_in_user
+        unless logged_in?
+            store_location
+            flash[:alert] = "Please log in."
+            redirect_to login_url
+        end
+    end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,28 @@
 class UsersController < ApplicationController
+    before_action :logged_in_user, only: [:show]
+    def show
+        @user = User.find(params[:id])
+    end
+    
+    def new
+        @user = User.new
+    end
+    
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            log_in @user
+            flash[:notice] = "Your account was created!"
+            redirect_to @user
+        else
+            render 'new'
+        end
+    end
+    
+    private
+
+    def user_params
+        params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    end 
+
 end
