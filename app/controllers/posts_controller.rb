a6ab838da21e13717153b7bbf036bcf92b28cf92
Adam Fleishaker <adamfleishaker@gmail.com>
2021-03-26 16:02:25 -0400

    Validation tweak for Heroku

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-    before_action :logged_in!, only: [:new, :show, :edit, :destroy]
+    before_action :logged_in!, only: [:index, :new, :show, :edit, :destroy]
 
     def index
         @posts = Post.all
