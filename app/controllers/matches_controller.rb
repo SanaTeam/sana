ecc6ef9f54ed9829d7f0ff1c9e42025bfcfe52f7
baumsarah <sarahbaum@brandeis.edu>
2021-04-06 17:22:28 -0400

    Matches index prompts log in

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,5 +1,5 @@
 class MatchesController < ApplicationController
-    before_action :logged_in!, only: [:show]
+    before_action :logged_in!, only: [:show, :index]
 
     def index
         redirect_to user_match_path(current_user.id)
