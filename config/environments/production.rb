664b94c4365414f20868587a59d2fe8db9f3e2c0
Adam Fleishaker <adamfleishaker@gmail.com>
2021-04-16 14:32:52 -0400

    Remove unnecessary flag

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,8 +84,6 @@ Rails.application.configure do
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
 
-  config.require_master_key = true
-
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
