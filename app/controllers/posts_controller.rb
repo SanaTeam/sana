d89b821e9f6297bf640e08fdae3775e836116cb0
Nicole Zamora <nicolebzf@brandeis.edu>
2021-04-16 15:43:39 -0400

    Added is_request attr to posts and show in forum

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,17 +2,15 @@ class PostsController < ApplicationController
     before_action :logged_in!, only: [:index, :new, :show, :edit, :destroy]
 
     def index
-        @categories = ["Financial Literacy", "Transportation", "Productivity", "Mindfulness", "Healthy Habits", "Other"]
+        @categories = ["Financial Literacy", "Transportation", "Productivity", "Mindfulness", "Healthy Habits", "Academics/Tutoring", "Language Learning", "Other"]
         search = params[:search].present? ? params[:search] : nil
         @posts = if search
-            puts "Search query"
             Post.search(search)
         else
-            puts "there was no search q"
             Post.all
         end
         @posts = @posts.select{|post| post.contains_categories?(params[:category])}
-        # @posts = @posts.order(created_at: :desc).page(params[:page])
+       
     end
 
     def search 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_09_183810) do
+ActiveRecord::Schema.define(version: 2021_04_16_190444) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 2021_04_09_183810) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "title"
     t.text "categories", default: [], array: true
+    t.boolean "is_request"
   end
 
   create_table "profiles", force: :cascade do |t|
