f1b7829e30a2c9564d7a6c9bee23f133a4b0321b
Adam Fleishaker <adamfleishaker@gmail.com>
2021-04-19 15:45:27 -0400

    Admin pinned/manage content, profile pictures, tweaks

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,5 @@
 module UsersHelper
+    def get_gravatar(user, classes, size)
+        image_tag("https://secure.gravatar.com/avatar/#{Digest::MD5::hexdigest(user.email.downcase)}", alt: user.full_name, class: classes, width: size)
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_14_040932) do
+ActiveRecord::Schema.define(version: 2021_04_19_191602) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +71,7 @@ ActiveRecord::Schema.define(version: 2021_04_14_040932) do
     t.string "password_digest"
     t.string "password_reset_token"
     t.datetime "password_reset_sent_at"
+    t.boolean "admin", default: false
   end
 
 end
