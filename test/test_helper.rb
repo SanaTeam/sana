3e2ced0761beaa2699868cc1999c980df49f624d
Nicole Zamora <nicolebzf@brandeis.edu>
2021-04-27 07:46:20 -0400

    edit minitest for searchkick and disabled_callback

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,6 +6,12 @@ class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
 
+  # reindex models for searchkick 
+  Post.reindex
+
+  # and disable callbacks
+  Searchkick.disable_callbacks
+
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
