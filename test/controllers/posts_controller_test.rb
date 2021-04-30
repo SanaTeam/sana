2cd4176454c3d5fd5df618724c83f9d73e9d897e
baumsarah <sarahbaum@brandeis.edu>
2021-04-29 21:19:35 -0400

    Added tests for creating post on behalf of org

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,6 +1,7 @@
 require "test_helper"
 require "minitest/reporters"
 require_relative '../helpers/NewUserHelper.rb'
+require_relative '../helpers/NewOrganizationHelper.rb'
 require_relative '../helpers/NewPostHelper.rb'
 
 Minitest::Reporters.use!
@@ -8,14 +9,22 @@ Minitest::Reporters.use!
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
   include NewUserHelper
+  include NewOrganizationHelper
   include NewPostHelper
 
   setup do
     new_user()
+    
     Searchkick.enable_callbacks
-    post_title = Faker::GreekPhilosophers.name
-    new_post(post_title, nil)
-    @post = Post.find_by(title: post_title)
+    organization_name = Faker::Company.name
+    new_organization(organization_name)
+    @organization = Organization.find_by(name: organization_name)
+    personal_post_title = "Post #1"
+    new_post(personal_post_title, nil)
+    @personal_post = Post.find_by(title: personal_post_title)
+    organization_post_title = "Post #2"
+    new_post(organization_post_title, @organization.id)
+    @organization_post = Post.find_by(title: organization_post_title)
   end
  
   def teardown
@@ -40,8 +49,14 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
     assert_template "show"
   end
   
-  test "should get edit view" do
-    get "/posts/#{@post.id}/edit"
+  test "should get personal post edit view" do
+    get "/posts/#{@personal_post.id}/edit"
+    assert_response :success
+    assert_template "edit"
+  end
+
+  test "should get organization post edit view" do
+    get "/posts/#{@organization_post.id}/edit"
     assert_response :success
     assert_template "edit"
   end
