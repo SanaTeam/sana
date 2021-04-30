be3b624ee6b23f85b369a6bd227cc8f6fc1390d8
baumsarah <sarahbaum@brandeis.edu>
2021-04-29 23:15:37 -0400

    Reinstated condition to ensure request match button doesn't show up when it shouldn't

--- test/controllers/matches_controller_test.rb
+++ test/controllers/matches_controller_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
+require_relative '../helpers/NewUserHelper.rb'
+require_relative '../helpers/NewOrganizationHelper.rb'
+require_relative '../helpers/NewPostHelper.rb'
+
+Minitest::Reporters.use!
 
 class MatchesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+    include NewUserHelper
+    include NewOrganizationHelper
+    include NewPostHelper
+    
 end
