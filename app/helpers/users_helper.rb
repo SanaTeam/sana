07db63891d8f5f37c6efc49d31c217388a960822
Adam Fleishaker <adamfleishaker@gmail.com>
2021-04-30 15:38:13 -0400

    Get gravatar

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,9 @@
 module UsersHelper
     def get_picture(user, classes, size)
-        url = "https://secure.gravatar.com/avatar/#{Digest::MD5::hexdigest(user.email.downcase)}?d=identicon"
         if user.picture
             url = user.picture
+        else
+            url = "https://secure.gravatar.com/avatar/#{Digest::MD5::hexdigest(user.email.downcase)}?d=identicon"
         end
         image_tag(url, alt: user.full_name, class: classes, width: size)
     end
