65ba537dcced74ff2be10adca54391d6a092b8f2
baumsarah <sarahbaum@brandeis.edu>
2021-05-07 19:12:31 -0400

    Resolved errors with sending replies and post show view

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -5,26 +5,30 @@ class RepliesController < ApplicationController
         redirect_to posts_url
     end
     def create
-        @post = Post.find(params[:id])
-        @reply = Reply.new(content: params[:content], to: params[:to], user_id: current_user.id, post_id: params[:id])
+        puts reply_params.inspect()
+        @post = Post.find(reply_params[:post_id].to_i)
+        @reply = Reply.new(content: reply_params[:content], to: reply_params[:to], user_id: current_user.id, post_id: @post.id)
         if @reply.save
             flash[:notice] = "Your reply was created!"
         else
             flash[:alert] = @reply.errors.full_messages.join(", ")
         end
-        redirect_to @post
+        redirect_to posts_url
     end
 
     def destroy
-        @reply = Reply.find(params[:id])
-        @post = Post.find(@reply.post_id)
+        @reply = Reply.find(reply_params[:id])
         @reply.destroy!
         flash[:notice] = "Your reply was destroyed!"
-        redirect_to @post
+        redirect_to posts_url
     end
 
     def new
         redirect_to posts_url
+    end
+
+    def reply_params
+        params[:reply] ? params[:reply] : params
     end
 
 end
